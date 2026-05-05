4a4d5f66971c098a88af72e1023cfb8f67642b11
gebinailasinaga <gebinailasinaga@example.com>
2026-05-05 21:31:07 +0700

    Update isi file model dan layout

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -8,7 +8,7 @@ class Pasien extends Model
 {
     // Tambahkan baris di bawah ini untuk proteksi Mass Assignment
     protected $fillable = [
-        'no_rekam_medis',
+        'no_rekam_medis',   
         'nama_pasien',
         'jenis_kelamin',
         'umur'
