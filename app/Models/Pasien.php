267898d105d4f8b77707d1ad339ee61d410659bf
gebinailasinaga <gebinailasinaga@example.com>
2026-05-05 21:21:28 +0700

    Menyelesaikan model pasien dan master layout

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pasien extends Model
 {
-    //
+    // Tambahkan baris di bawah ini untuk proteksi Mass Assignment
+    protected $fillable = [
+        'no_rekam_medis',
+        'nama_pasien',
+        'jenis_kelamin',
+        'umur'
+    ];
 }
